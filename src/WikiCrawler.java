44d242f92a6a1b3449d60247da387cbf81c5606f
Yuxiang Chen <yuxiangc@iastate.edu>
2017-03-29 15:52:10 -0500

    Finish extract links

--- src/WikiCrawler.java
+++ src/WikiCrawler.java
@@ -1,8 +1,11 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -28,7 +31,47 @@ public class WikiCrawler {
     public ArrayList<String> extractLinks(String doc) {
         //TODO
 
+        Hashtable<Integer, String> hashtable = new Hashtable<Integer, String>();
+
         boolean findP = false;
+
+        ArrayList<String> ret = new ArrayList<>();
+
+        Scanner scanner = new Scanner(doc);
+
+        while (scanner.hasNextLine()) {
+
+            String s = scanner.nextLine();
+
+            if (!findP) {
+                if (Pattern.matches("^<p>.*", s)) {
+                    findP = true;
+                } else {
+                    continue;
+                }
+            }
+
+            Pattern p = Pattern.compile("<a href=\"/wiki/\\w+\"");
+            Matcher m = p.matcher(s);
+            while (m.find()) {
+                String output = m.group();
+                output = output.substring(9, output.length() - 1);
+
+                if (hashtable.contains(output)) {
+                    continue;
+                } else {
+                    hashtable.put(output.hashCode(), output);
+                    ret.add(output);
+                    System.out.println(output);
+                }
+            }
+        }
+
+
+        return ret;
+    }
+
+    public void crawl() {
 
         try {
 
@@ -36,36 +79,24 @@ public class WikiCrawler {
             InputStream is = url.openStream();
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
-            String s = br.readLine();
+            String doc = ReadBigStringIn(br);
+            extractLinks(doc);
 
-            while (s != null) {
-
-                if (!findP) {
-                    if (Pattern.matches("^<p>.*", s)) {
-                        findP = true;
-                    } else {
-                        s = br.readLine();
-                        continue;
-                    }
-                }
-
-                Pattern p = Pattern.compile("<a href=\"/wiki/\\w+\"");
-                Matcher m = p.matcher(s);
-                while (m.find())
-                    System.out.println(m.group());
-
-                s = br.readLine();
-            }
-
+            //TODO
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
+
     }
 
-    public void crawl() {
-        //TODO
+    private String ReadBigStringIn(BufferedReader buffIn) throws IOException {
+        StringBuilder everything = new StringBuilder();
+        String line;
+        while ((line = buffIn.readLine()) != null) {
+            everything.append(line + '\n');
+        }
+        return everything.toString();
     }
 
 
