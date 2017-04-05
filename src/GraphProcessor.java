254b8d2782529fc0fc0ace234401646842cca470
chen8160 <chenyuxiang8160@gmail.com>
2017-04-05 13:12:13 -0500

    Finish SCC algo

--- src/GraphProcessor.java
+++ src/GraphProcessor.java
@@ -1,3 +1,5 @@
+import com.sun.corba.se.spi.ior.IdentifiableFactory;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,12 +11,17 @@ import java.util.Scanner;
  */
 public class GraphProcessor {
 
-    private int max;
+    private int numVertices;
+    private int numComponents;
+    private int largestSize;
     private HashMap<String, ArrayList<String>> graph;
     private HashMap<String, ArrayList<String>> graphR;
-    private HashMap<String, HashSet<String>> components;
+    private HashMap<String, ArrayList<String>> components;
 
     public GraphProcessor(String graphData) {
+
+        numComponents = 0;
+        largestSize = 0;
         File f = new File(graphData);
         graph = new HashMap<>();
         graphR = new HashMap<>();
@@ -25,7 +32,7 @@ public class GraphProcessor {
             Scanner scanner = new Scanner(f);
 
             if (scanner.hasNextInt()) {
-                max = scanner.nextInt();
+                numVertices = scanner.nextInt();
                 scanner.nextLine();
             }
 
@@ -37,14 +44,14 @@ public class GraphProcessor {
                 String u = lineScan.next();
 
                 ArrayList<String> out = graph.get(v);
-                if (out == null){
+                if (out == null) {
                     out = new ArrayList<>();
                     graph.put(v, out);
                 }
                 out.add(u);
 
                 ArrayList<String> outR = graphR.get(u);
-                if (outR == null){
+                if (outR == null) {
                     outR = new ArrayList<>();
                     graphR.put(u, outR);
                 }
@@ -61,26 +68,28 @@ public class GraphProcessor {
 
     public int outDegree(String v) {
         ArrayList<String> out = graph.get(v);
-        if (out != null){
+        if (out != null) {
             return out.size();
         }
         return 0;
     }
 
     public boolean sameComponent(String u, String v) {
+        if (components.get(u) != null)
+            return components.get(u).contains(v);
         return false;
     }
 
     public ArrayList<String> componentVertices(String v) {
-        return null;
+        return components.get(v);
     }
 
     public int lagestComponent() {
-        return 0;
+        return largestSize;
     }
 
     public int numComponents() {
-        return 0;
+        return numComponents;
     }
 
     public ArrayList<String> bfsPath(String u, String v) {
@@ -89,25 +98,44 @@ public class GraphProcessor {
 
 
     //Private Methods
-    private void SCC(){
+    private void SCC() {
 
         HashSet<String> mark = new HashSet<>();
-        String[] finishTime = new String[max];
+        String[] finishTime = new String[numVertices];
         computeOrder(mark, finishTime);
 //        System.out.print("");
         mark.clear();
 
-        for (int i = max - 1; i >= 0; i--){
+        for (int i = numVertices - 1; i >= 0; i--) {
             String v = finishTime[i];
-            if (!mark.contains(v)){
-                HashSet<String> component = new HashSet<>();
+            if (!mark.contains(v)) {
+                ArrayList<String> component = new ArrayList<>();
+                sccDFS(mark, component, v);
+                numComponents++;
+                largestSize = Math.max(largestSize, component.size());
             }
         }
 
 
     }
 
-    private void computeOrder(HashSet<String> mark, String[] finishTime){
+    private void sccDFS(HashSet<String> mark, ArrayList<String> component, String v) {
+        mark.add(v);
+        component.add(v);
+        components.put(v, component);
+        ArrayList<String> edges = graph.get(v);
+        if (edges == null) {
+            return;
+        }
+        for (String u : edges) {
+            if (!mark.contains(u)) {
+                sccDFS(mark, component, u);
+            }
+        }
+
+    }
+
+    private void computeOrder(HashSet<String> mark, String[] finishTime) {
 
         mark.clear();
         int counter = 0;
@@ -118,11 +146,11 @@ public class GraphProcessor {
         }
     }
 
-    private int finishDFS(HashSet<String> mark, String v, int counter, String[] finishTime){
+    private int finishDFS(HashSet<String> mark, String v, int counter, String[] finishTime) {
 
         mark.add(v);
-        for (String u : graphR.get(v)){
-            if (!mark.contains(u)){
+        for (String u : graphR.get(v)) {
+            if (!mark.contains(u)) {
                 counter = finishDFS(mark, u, counter, finishTime);
             }
         }
